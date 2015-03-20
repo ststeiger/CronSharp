ce467355ca3b05c09e9b0b79495588175be64410
ststeiger <quandary82@hailmail.net>
2015-03-20 16:33:44 +0100

    Added Ajax

---
+++ CronManager/ajax/JSON.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Tools
+{
+
+
+    public class JSON
+    {
+
+
+        public static string Serialize(object obj)
+        {
+            return Serialize(obj, true);
+        }
+
+
+        public static string Serialize(object obj, bool prettyPrint)
+        {
+
+            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings { NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore };
+            if (!prettyPrint)
+            {
+                settings.Formatting = Newtonsoft.Json.Formatting.None;
+            }
+            else
+            {
+                settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            }
+            settings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+
+
+            string strResult = Newtonsoft.Json.JsonConvert.SerializeObject(obj, settings);
+            System.Console.WriteLine(strResult);
+            return strResult;
+        }
+
+
+    }
+
+
+}
---
+++ CronManager/ajax/SaveConfig.ashx.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Web;
+
+
+namespace CronManager.ajax
+{
+
+
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für SaveConfig
+    /// </summary>
+    public class SaveConfig : IHttpHandler
+    {
+
+
+        public class cAjaxResult
+        {
+            public bool isError = false;
+            public string ErrorMessage;
+            public string cronTab;
+        }
+
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //context.Response.ContentType = "text/plain";
+            context.Response.ContentType = "application/json";
+            cAjaxResult AjaxResult = new cAjaxResult();
+
+            try
+            {
+                AjaxResult.cronTab = context.Request.Params["crontab"];
+            }
+            catch (System.Exception ex)
+            {
+                AjaxResult.ErrorMessage = ex.Message;
+            }
+
+            // string strResult = "bla bla";
+            string strResult = Tools.JSON.Serialize(AjaxResult);
+
+            string strBasePath = "";
+
+            string strCurrentDirectory = System.Web.Hosting.HostingEnvironment.IsHosted ? System.Web.Hosting.HostingEnvironment.MapPath("~")
+                : System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(strCurrentDirectory);
+            strBasePath = System.IO.Path.Combine(di.Parent.FullName, "CronSharp");
+            strBasePath = System.IO.Path.Combine(strBasePath, "CronSharp.ini");
+
+            System.IO.File.WriteAllText(strBasePath, AjaxResult.cronTab, System.Text.Encoding.UTF8);
+
+            System.Console.WriteLine(strResult);
+            context.Response.Write(strResult);
+        }
+
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+
+    }
+
+
+}
